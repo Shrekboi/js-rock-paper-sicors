2e0b3533d98f3d16e72a92a4d6932a5ff2abf658
Shrekboi <programming.shek@gmail.com>
2021-12-31 20:53:23 +0200

    random update

--- js.js
+++ js.js
@@ -1,5 +1,8 @@
-let playerSelection = "paper"
+
 let computerChoise;
+let playerSelection;
+let userScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
     let number = Math.random();
@@ -17,16 +20,73 @@ function computerPlay() {
     return computerChoise
 }
 
-// function game(computer, player) {
-//     if (number == playerSelection) {
-//         console.log("draw")
-//     }
-//     else if 
+function playRound(computer, player) {
+    computerPlay()
+    let playerSelectionWut = prompt("rock, paper or sicors")
+    let playerSelection = playerSelectionWut.toLocaleLowerCase();
+    // console.log(computer)
+    // console.log(player)
 
-// }
+    if (computer === playerSelection) {
+        return ("draw")
+    }
+    else if (playerSelection == "sicors") {
+        if (computer == "rock") {
+            computerScore++;
+            return ("you lose")
+        }
+        else {
+            userScore++;
+            return ("you win")
+        }
+    }
+    else if (playerSelection == "paper") {
+        if (computer == "rock") {
+            userScore++;
+            return ("you win")
+        }
+        else {
+            computerScore++;
+            return ("you lose")
+        }
+    }
+    else if (playerSelection == "rock") {
+        if (computer == "sicors") {
+            userScore++;
+            return ("you win")
+        }
+        else {
+            computerScore++;
+            return ("you lose")
+        }
+    }
 
-computerPlay()
-console.log(computerChoise)
+    console.log("your choise ", playerSelection)
+    console.log("computer choise ", computer)
+
+}
+
+function game() {
+    console.log(playRound(computerChoise, playerSelection))
+    console.log(playRound(computerChoise, playerSelection))
+    console.log(playRound(computerChoise, playerSelection))
+    console.log(playRound(computerChoise, playerSelection))
+    console.log(playRound(computerChoise, playerSelection))
+
+    if (userScore > computerScore) {
+        console.log("YOU WON THE GAME")
+    }
+    else if (computerScore > userScore) {
+        console.log("BOI YOU HAVE LOST")
+    }
+
+}
+
+
+game()
+
+
+
 
 
 
