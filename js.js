9098a3ad1fde506dabfec5aab44ab7671dda0d60
Shrekboi <programming.shek@gmail.com>
2021-12-31 22:44:46 +0200

    got the buttons wokring

--- js.js
+++ js.js
@@ -1,10 +1,9 @@
-
 let computerChoise;
 let playerSelection;
 let userScore = 0;
 let computerScore = 0;
 
-function computerPlay() {
+function computerPlay() {  //computer chooses 
     let number = Math.random();
 
     if (number < 0.33) {
@@ -21,16 +20,18 @@ function computerPlay() {
 }
 
 function playRound(computer, player) {
-    computerPlay()
-    let playerSelectionWut = prompt("rock, paper or sicors")
-    let playerSelection = playerSelectionWut.toLocaleLowerCase();
-    // console.log(computer)
-    // console.log(player)
+    
+    console.log(computer)
+    console.log(player)
 
-    if (computer === playerSelection) {
+
+    const computer_choisee = document.querySelector("#cchoise")
+    computer_choisee.textContent = "nigag";
+
+    if (computer === player) {
         return ("draw")
     }
-    else if (playerSelection == "sicors") {
+    else if (player == "sicors") {
         if (computer == "rock") {
             computerScore++;
             return ("you lose")
@@ -40,7 +41,7 @@ function playRound(computer, player) {
             return ("you win")
         }
     }
-    else if (playerSelection == "paper") {
+    else if (player == "paper") {
         if (computer == "rock") {
             userScore++;
             return ("you win")
@@ -50,7 +51,7 @@ function playRound(computer, player) {
             return ("you lose")
         }
     }
-    else if (playerSelection == "rock") {
+    else if (player == "rock") {
         if (computer == "sicors") {
             userScore++;
             return ("you win")
@@ -61,24 +62,46 @@ function playRound(computer, player) {
         }
     }
 
-    console.log("your choise ", playerSelection)
-    console.log("computer choise ", computer)
-
+    const computer_choise = document.querySelector("#cchoise")
+    computer_choise.textContent = "nigag";
 }
 
 function game() {
-    console.log(playRound(computerChoise, playerSelection))
-    console.log(playRound(computerChoise, playerSelection))
-    console.log(playRound(computerChoise, playerSelection))
-    console.log(playRound(computerChoise, playerSelection))
-    console.log(playRound(computerChoise, playerSelection))
 
-    if (userScore > computerScore) {
-        console.log("YOU WON THE GAME")
-    }
-    else if (computerScore > userScore) {
-        console.log("BOI YOU HAVE LOST")
-    }
+    const player_choise = document.querySelector("#pchoise");
+    const computer_choisee = document.querySelector("#cchoise")
+    
+
+    const rock_btn = document.querySelector("#rockbtn");
+    rock_btn.addEventListener("click", () => {
+        playRound(computerPlay(), "rock");
+        player_choise.textContent = "rock"
+        computer_choisee.textContent = computerChoise;
+
+    })
+
+
+    const paper_btn = document.querySelector("#paperbtn");
+    paper_btn.addEventListener("click", () => {
+        playRound(computerPlay(), "paper");
+        player_choise.textContent = "paper"
+        computer_choisee.textContent = computerChoise;
+
+    })
+
+    const sicors_btn = document.querySelector("#sicorsbtn");
+    sicors_btn.addEventListener("click", () => {
+        playRound(computerPlay(), "sicors");
+        player_choise.textContent = "sicors"
+        computer_choisee.textContent = computerChoise;
+    })
+
+    // if (userScore > computerScore) {
+    //     console.log("YOU WON THE GAME")
+    // }
+    // else if (computerScore > userScore) {
+    //     console.log("BOI YOU HAVE LOST")
+    // }
 
 }
 
