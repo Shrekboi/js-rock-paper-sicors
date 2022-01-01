a1c0ffb3ebc7ad3dc7c0339b028be101d3f1d741
Shrekboi <programming.shek@gmail.com>
2022-01-01 18:38:41 +0200

    game should work ok with a few bugs

--- js.js
+++ js.js
@@ -3,6 +3,14 @@ let playerSelection;
 let userScore = 0;
 let computerScore = 0;
 
+const player_choise = document.querySelector("#pchoise");
+const computer_choisee = document.querySelector("#cchoise")
+const rock_btn = document.querySelector("#rockbtn");
+const displayPlayerInfo = document.querySelector(".pscore")
+const displayComputerInfo = document.querySelector(".cscore")
+const paper_btn = document.querySelector("#paperbtn");
+const sicors_btn = document.querySelector("#sicorsbtn");
+
 function computerPlay() {  //computer chooses 
     let number = Math.random();
 
@@ -19,90 +27,77 @@ function computerPlay() {  //computer chooses
     return computerChoise
 }
 
+function resetScore() {  // funcion to reset the scores
+    computerScore = 0;
+    userScore = 0;
+    displayPlayerInfo.textContent = `${userScore}`
+    displayComputerInfo.textContent = `${computerScore}`
+    player_choise.textContent = "your choice"; //these don't work
+    computer_choisee.textContent = "computer choise"; // fix em later
+}
+
+function CheckIfGameOver() {
+    if (computerScore == 5 || userScore == 5) {  // check if game is over
+        if (userScore > computerScore) {
+            alert("BOI YOU WON THE GAME")
+            resetScore();
+        }
+        else if (computerScore > userScore) {
+            alert("BOI YOU HAVE LOST")
+            resetScore();
+        }
+    }
+}
+
 function playRound(computer, player) {
-    
-    console.log(computer)
-    console.log(player)
 
-
-    const computer_choisee = document.querySelector("#cchoise")
-    computer_choisee.textContent = "nigag";
-
-    if (computer === player) {
-        return ("draw")
-    }
-    else if (player == "sicors") {
-        if (computer == "rock") {
-            computerScore++;
-            return ("you lose")
+    if (computerScore == 5 || userScore == 5) {  // check if game is over
+        if (userScore > computerScore) {
+            alert("BOI YOU WON THE GAME")
+            resetScore();
         }
-        else {
-            userScore++;
-            return ("you win")
-        }
-    }
-    else if (player == "paper") {
-        if (computer == "rock") {
-            userScore++;
-            return ("you win")
-        }
-        else {
-            computerScore++;
-            return ("you lose")
-        }
-    }
-    else if (player == "rock") {
-        if (computer == "sicors") {
-            userScore++;
-            return ("you win")
-        }
-        else {
-            computerScore++;
-            return ("you lose")
+        else if (computerScore > userScore) {
+            alert("BOI YOU HAVE LOST")
+            resetScore();
         }
     }
 
-    const computer_choise = document.querySelector("#cchoise")
-    computer_choise.textContent = "nigag";
+    if (player == "rock" && computer == "sicors" ||
+        player == "paper" && computer == "rock" ||
+        player == "sicors" && computer == "paper") {
+        userScore++;
+        displayPlayerInfo.textContent = `${userScore}`
+    }
+    else if (player == "rock" && computer == "paper" ||
+        player == "paper" && computer == "sicors" ||
+        player == "sicors" && computer == "rock") {
+        computerScore++;
+        displayComputerInfo.textContent = `${computerScore}`
+    }
+
+    CheckIfGameOver();
+
 }
 
 function game() {
 
-    const player_choise = document.querySelector("#pchoise");
-    const computer_choisee = document.querySelector("#cchoise")
-    
-
-    const rock_btn = document.querySelector("#rockbtn");
     rock_btn.addEventListener("click", () => {
         playRound(computerPlay(), "rock");
         player_choise.textContent = "rock"
         computer_choisee.textContent = computerChoise;
-
     })
 
-
-    const paper_btn = document.querySelector("#paperbtn");
     paper_btn.addEventListener("click", () => {
         playRound(computerPlay(), "paper");
         player_choise.textContent = "paper"
         computer_choisee.textContent = computerChoise;
-
     })
 
-    const sicors_btn = document.querySelector("#sicorsbtn");
     sicors_btn.addEventListener("click", () => {
         playRound(computerPlay(), "sicors");
         player_choise.textContent = "sicors"
         computer_choisee.textContent = computerChoise;
     })
-
-    // if (userScore > computerScore) {
-    //     console.log("YOU WON THE GAME")
-    // }
-    // else if (computerScore > userScore) {
-    //     console.log("BOI YOU HAVE LOST")
-    // }
-
 }
 
 game()
